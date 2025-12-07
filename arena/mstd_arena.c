b5f33958d035a7dc1d307fcf7ee20d08bd4331cc
BlueMoonHunt <hkumar.bluemoonhunt@gmail.com>
2025-12-07 20:04:01 +0530

    started arena implementation

--- arena/mstd_arena.c
+++ arena/mstd_arena.c
@@ -1,3 +1,38 @@
-void into() {
-    volatile int a = 0;
+#include "mstd_arena.h"
+#include <os/mstd_os.h>
+
+Arena* arena_alloc(const ArenaCreateInfo create_info) {
+    u64 reserve_size = create_info.reserve_size;
+    u64 commit_size = create_info.commit_size;
+
+    if(create_info.allocate_large_pages) {
+        reserve_size = align_up_pow2(reserve_size, os_get_large_page_size());
+        commit_size = align_up_pow2(commit_size, os_get_large_page_size());
+    }
+    else {
+        reserve_size = align_up_pow2(reserve_size, os_get_page_size());
+        commit_size = align_up_pow2(commit_size, os_get_page_size());
+    }
+
+    void* base;
+    if(create_info.allocate_large_pages) {
+        base = os_reserve_large(reserve_size);
+        os_commit_large(base, commit_size);
+    }
+    else {
+        base = os_reserve(reserve_size);
+        os_commit(base, commit_size);
+    }
+
+    Arena* arena = (Arena*)base;
+    arena->reserve_size = reserve_size;
+    arena->commit_size = commit_size;
+    arena->base = (Arena*)base + 1;
+    arena->offset = 0;
+
+    return arena;
+}
+
+void arena_release(Arena* arena) {
+    os_release(arena, arena->reserve_size);
 }
--- arena/mstd_arena.h
+++ arena/mstd_arena.h
@@ -1,6 +1,6 @@
 #ifndef MSTD_ARENA_IMPLEMENTATION
 #define MSTD_ARENA_IMPLEMENTATION
-#include <core/mstd_types.h>
+#include <core/mstd_core.h>
 
 typedef struct ArenaCreateInfo ArenaCreateInfo;
 struct ArenaCreateInfo {
@@ -11,12 +11,13 @@ struct ArenaCreateInfo {
 
 typedef struct Arena Arena;
 struct Arena {
-    u64 cmt_size;
-    u64 res_size;
-    u64 base_pos;
-    u64 pos;
-    u64 cmt;
-    u64 res;
+    u64 reserve_size;
+    u64 commit_size;
+    size_t base;
+    size_t offset;
 };
 
+
+Arena* arena_alloc(const ArenaCreateInfo create_info);
+void arena_release(Arena* arena);
 #endif // MSTD_ARENA_IMPLEMENTATION
